5d60605adff7c3c49f67da10d0542449b2e255d2
om prakash <omprakashkumarsantosh@gmail.com>
2025-04-17 21:17:55 +0530

    connect with backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://book-review-system-cc20.onrender.com",
     withCredentials:true,
 })
 
